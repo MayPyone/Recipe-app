9ed03c9c2f5396a92ca95f8ae983ac00b4666502
May Pyone <maypyone015@gmail.com>
2023-12-08 16:57:31 +0630

    Test for food controller

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,46 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe 'Foods', type: :request do
+  include Devise::Test::IntegrationHelpers
+
   before do
-    @user1 = User.create!(id: 5, name: 'Bijay',
-                          photo: 'https://c0.klipartz.com/pngpicture/695/991/gratis-png-profesor.png')
-    @user2 = User.create!(id: 6, name: 'May',
-                          photo: 'https://c0.klipartz.com/pngpicture/695/991/gratis-png-profesor.png')
-    @post1 = Post.create!(title: 'Intro', text: 'Hi', comment_counter: 0, like_counter: 2, author: @user1)
-    Post.create!(title: 'Intro', text: 'Hi', comment_counter: 0, like_counter: 2, author: @user1)
-    Post.create!(title: 'Intro', text: 'Hi', comment_counter: 0, like_counter: 2, author: @user2)
+    @user = User.create!(name: 'may', email: 'maypyone@gmail.com', password: '123456', id: '1')
+    @food = Food.create!(user_id: @user.id, name: 'chicken')
+    sign_in @user
   end
+
   describe 'GET /index' do
     it 'returns http success' do
-      get '/users'
+      get root_path
       expect(response).to have_http_status(:success)
     end
+
+    it 'render template' do
+        get root_path
+        expect(response).to render_template(:index)
+      end
   end
+
+
 
   describe 'GET /show' do
     it 'returns http success' do
-      get "/users/#{@user1.id}"
+      get foods_path(@food)
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'View for users' do
-    it 'show all the included elements from the index page and render the template' do
-      get '/users'
-      expect(response.body).to include('number of posts:')
-
-      expect(response.body).to render_template('index')
-    end
-
-    it 'render the show page' do
-      get "/users/#{@user1.id}"
-      expect(response.body).to render_template('show')
-      expect(response.body).to include("<p class='num-post'>Number of posts: 2</p>")
-    end
-
-    it 'mock one element' do
-      get '/users'
-      assert_includes response.body, @user1.id.to_s
-    end
+ describe 'Get #create' do
+ it 'returns http success' do
+    get new_food_path
+    expect(response).to have_http_status(:success)
   end
+end
+
+describe 'DELETE #destroy' do
+it 'returns http success' do
+    delete food_path(@food.id)
+    expect(response).to redirect_to(foods_path)
+  end
+end
+
 end
