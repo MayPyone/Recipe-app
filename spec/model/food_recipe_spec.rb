a8b859a6f41c0fcc409ce95b82fb1d79e13d5929
May Pyone <maypyone015@gmail.com>
2023-12-08 13:35:35 +0630

    Test for food-recipe model

---
+++ spec/model/food_recipe_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe FoodRecipe, type: :model do
+  describe 'associations' do
+    it 'belongs to a reciipe' do
+      association = FoodRecipe.reflect_on_association(:reciipe)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it 'belongs to a food' do
+      association = FoodRecipe.reflect_on_association(:food)
+      expect(association.macro).to eq(:belongs_to)
+    end
+  end
+end
